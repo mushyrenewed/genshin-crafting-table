4872d445f2a51fa11e0e9b0ab5d4601f3b1fc734
Arjun Cosare <arjun.cosare@outlook.com>
2021-01-25 14:09:40 -0700

    replace buttonmenu for more general btn

--- code/genshin-crafting-table/src/components/MenuItem.vue
+++ code/genshin-crafting-table/src/components/MenuItem.vue
@@ -1,29 +1,55 @@
 <template>
   <div class="menuItemClass d-flex">
     <diamond />
-    <menu-button :label="label" class="ml-4" :handler="handler"/>
+    <button-genshin :buttonHandler="handler" class="ml-4 w-100">
+      <div class="buttonMenuClass">
+        <span>{{ label }}</span>
+      </div>
+    </button-genshin>
+    
   </div>
 </template>
 
 <script>
 import diamond from "./Diamond.vue";
-import MenuButton from "./ButtonMenu.vue";
+import ButtonGenshin from "./ButtonGenshin.vue";
+
 
 export default {
-    props:{
-        label: String,
-        handler: Function
-    },
+  props: {
+    label: String,
+    handler: Function,
+  },
   components: {
     diamond,
-    MenuButton,
+    ButtonGenshin,
   },
 };
 </script>
 
 <style scoped>
-.menuItemClass:hover > .diamondOuterClass{
-    border-color: white;
+.menuItemClass:hover > .diamondOuterClass {
+  border-color: white;
+}
+
+.buttonMenuClass > span {
+  color: white;
+}
+.buttonMenuClass {
+  border-radius: 2.5px;
+  padding: 10px;
+  width: 100%;
+  cursor: pointer;
+}
+
+.buttonMenuClass:hover,
+.buttonMenuClass:focus {
+  background-color: white;
+}
+
+.buttonMenuClass:hover > span,
+.buttonMenuClass:focus > span {
+  color: #04376e;
 }
 </style>
 
